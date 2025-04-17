51873417f429aa033277a61851186a4076352bfe
ZenithSUS <jeranmerino147@gmail.com>
2025-04-17 11:12:53 +0800

    delete hook user

--- src/hooks/users.ts
+++
@@ -1,8 +0,0 @@
-import { updateExistingUser } from "../actions/users";
-import { useMutation, UseBaseMutationResult } from "@tanstack/react-query";
-import { UpdateUserProfile } from "../utils/types";
-
-export const useUpdateUser = (): UseBaseMutationResult<UpdateUserProfile> => {
-    return useMutation(updateExistingUser, {
-        onSuccess: () => {},
-    });
