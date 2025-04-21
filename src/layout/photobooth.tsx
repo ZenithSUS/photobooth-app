339eaed3f3aada31fd27629762cb0fe62bcceae2
ZenithSUS <jeranmerino147@gmail.com>
2025-04-21 16:05:30 +0800

    refactor: streamline authentication logic in MainLayout component

--- src/layout/photobooth.tsx
+++ src/layout/photobooth.tsx
@@ -1,30 +1,24 @@
 import { Outlet } from "react-router-dom";
-import { account } from "../appwrite";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+
 import { Navigate } from "react-router-dom";
 import fetchAuthUser from "../lib/services/getAuth";
-import Loading from "../components/ui/loading";
 
 export default function MainLayout() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isAuthChecking, setIsAuthChecking] = useState(true);
+  let user = JSON.parse(localStorage.getItem("session") || "false") as boolean;
+  const guestMode = !user;
 
   useEffect(() => {
     if (guestMode) return;
     const getAuth = async () => {
-      user = await fetchAuthUser(user);
+      user = await fetchAuthUser(user as boolean);
     };
     getAuth();
   }, []);
 
-  if (isAuthChecking) {
-    return <Loading />;
-  }
-
-  if (!isAuthenticated) {
+  if (!user && !guestMode) {
     return <Navigate to="/login" />;
   }
-
   <Outlet />;
   return (
     <main className="grid min-h-dvh grid-rows-[auto_1fr]">
